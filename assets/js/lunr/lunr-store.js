a79a06a1a8d0deee0a9481396e18e4a42b9146ca
mkuthan <mkuthan@users.noreply.github.com>
2021-07-13 20:00:07 +0000

    jekyll build from Action faaf57bf59132809b397fc16d62e3c9556c6187a

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,157 @@
+var store = [{
+        "title": "Artifactory Performance Tuning",
+        "excerpt":"Few years ago I participated in Kirk Pepperdine Java performance tuning training. One of the greatest technical training which I have ever been! And also great opportunity to visit Crete :-) Let’s check what I remember from the training … In this blog post I would like to show Artifactory...","categories": ["artifactory","jvm","performance","linux"],
+        "tags": [],
+        "url": "/blog/2011/03/10/artifactory-performance-tuning/",
+        "teaser": null
+      },{
+        "title": "Pure JEE or Spring Framework",
+        "excerpt":"During my career as J2EE and JEE software developer I have been trying to use pure JEE two o three times. And I decided to do not repeat this exercise any more, it would be waste of my precious time. Below you can find short but quite comprehensive summary (based...","categories": ["java","spring","JEE"],
+        "tags": [],
+        "url": "/blog/2012/05/31/pure-jee-or-spring-framework/",
+        "teaser": null
+      },{
+        "title": "How to fail a software project fast and efficient?",
+        "excerpt":"The short list based on excellent Thomas Sundberg presentation. Divide the team into separate divisions - analysis, architecture, development, operations, test, etc. Make the communication as hard as possible, different locations, different managers, different cost centres etc. Assign developer to multiple projects managed by different people Developers will be able...","categories": ["craftsmanship"],
+        "tags": [],
+        "url": "/blog/2012/06/01/how-to-fail-software-project-fast-and-efficient/",
+        "teaser": null
+      },{
+        "title": "How to convince your manager to adopt Git",
+        "excerpt":"Distributed Concurrent Versions Systems (DCVSs) like Git or Mercurial has changed software delivery processes significantly. I would not want to go back to the mid ages of Subversion, and I’m able to convince almost any developer to use DCVS. Convincing managers is much more tough task. Below I collected some...","categories": ["DVCS","git"],
+        "tags": [],
+        "url": "/blog/2012/11/13/how-to-convince-your-manager-to-adopt/",
+        "teaser": null
+      },{
+        "title": "Code coverage for managers and developers",
+        "excerpt":"From time to time, people ask me what code coverage by tests should be. Does 60% mean that project is healthy? Or maybe the goal should be 70% or 80%? I don’t know, it depends on your role in the project If you are a manager, Excel is your friend....","categories": ["craftsmanship"],
+        "tags": [],
+        "url": "/blog/2012/12/03/code-coverage-for-managers-and-developers/",
+        "teaser": null
+      },{
+        "title": "Virtual Box VDI maintanance",
+        "excerpt":"Virtual Disk Image (VDI) is a Virtual Box container format for guest hard disk. I found that VDI files on the host system grows over the time. If your VDI file on the host system is much bigger than used spaces on guest partition it is time for compaction: Install...","categories": ["linux","virtual box"],
+        "tags": [],
+        "url": "/blog/2012/12/11/virtual-box-vdi-maintanance/",
+        "teaser": null
+      },{
+        "title": "How to document your professional experiences",
+        "excerpt":"Have you considered what is important for prospective employer? What is the most valuable information source about your professional experience? How to document that you are an expert in software engineering? Below you can find some of my tricks: Write a blog, teaching is the best learning method :-) Write...","categories": ["craftsmanship"],
+        "tags": [],
+        "url": "/blog/2013/03/21/how-to-document-your-professional/",
+        "teaser": null
+      },{
+        "title": "GitFlow step by step",
+        "excerpt":"Git Flow is a mainstream process for branch per feature development. Git Flow is the best method I’ve found for managing project developed by small to medium project teams. Before you start reading this post you should read two mandatory lectures: Git Workflows by Atlassian Maven JGit-Flow Plugin This blog...","categories": ["git","maven","jira"],
+        "tags": [],
+        "url": "/blog/2013/07/21/gitflow-step-by-step/",
+        "teaser": null
+      },{
+        "title": "Development Environment Setup",
+        "excerpt":"This document is a manual how to configure flexible development environment for Java, JavaScript, Ruby and Python - my primary set of tools. Even if the runtimes installation with apt-get seems to be a trivial task, there is limited control over installed version of the runtime. The goal is to...","categories": ["linux","bash","ruby","java","node.js","python"],
+        "tags": [],
+        "url": "/blog/2013/10/09/development-environment-setup/",
+        "teaser": null
+      },{
+        "title": "DDD Architecture Summary",
+        "excerpt":"In this blog post you can find my general rules for implementing system using Domain Driven Design. Do not use them blindly but it is good starting point for DDD practitioners. Bounded Context Separate bounded context for each important module of the application (important from business partner perspective). Independent of...","categories": ["DDD","architecture","spring"],
+        "tags": [],
+        "url": "/blog/2013/11/04/ddd-architecture-summary/",
+        "teaser": null
+      },{
+        "title": "How to send email from JEE application",
+        "excerpt":"Sending email notifications from enterprise application is very common scenario. I know several methods to solve this puzzle, below you can find short summary. To send an email from the application at least SMTP server address must be configured. Because released application binary (e.g: WAR file) should be portable across...","categories": ["JMS","smtp","spring"],
+        "tags": [],
+        "url": "/blog/2013/12/06/how-to-send-email-from-jee-application/",
+        "teaser": null
+      },{
+        "title": "The Twelve-Factor App - part 1",
+        "excerpt":"During my studies about “Micro Services” I found comprehensive (but short) document about Twelve-Factor App methodology for building software-as-a-service applications. The orginal paper is published at 12factor.net. Below you can find a short summary of my experiences for the first part of the document. There is also a second part...","categories": ["architecture"],
+        "tags": [],
+        "url": "/blog/2014/05/26/the-twelve-factor-app-part1/",
+        "teaser": null
+      },{
+        "title": "The Twelve-Factor App - part 2",
+        "excerpt":"This blog post is a continuation of first part of this blog series. 7. Port Binding The twelve-factor app is completely self-contained and does not rely on runtime injection of a webserver into the execution environment to create a web-facing service. I developed self-contained web application once, with embedded Jetty...","categories": ["architecture"],
+        "tags": [],
+        "url": "/blog/2014/05/27/the-twelve-factor-app-part2/",
+        "teaser": null
+      },{
+        "title": "Acceptance testing using JBehave, Spring Framework and Maven",
+        "excerpt":"This post documents acceptance testing best practices collected in regular projects I was working on. Best practices materialized into working project, using Jbehave, Spring Framework and Maven. After the lecture you will know: How to implement automated acceptance tests and avoid common traps. How to organize project build using Maven....","categories": ["jbehave","spring","maven"],
+        "tags": [],
+        "url": "/blog/2014/05/29/acceptance-testing-using-jbehave-spring-framework-and-maven/",
+        "teaser": null
+      },{
+        "title": "Rrelease It! - book review",
+        "excerpt":"Recently I read excellent book Release It! written by Michael Nygard. The book is 7 years old and I don’t know how I could miss the book until now. Michael Nygard shows how to design and architect medium or large scale web applications. Real lessons learnt from the trenches not...","categories": ["books"],
+        "tags": [],
+        "url": "/blog/2014/06/21/release-it-book-review/",
+        "teaser": null
+      },{
+        "title": "SOA Patterns - book review",
+        "excerpt":"Overview I took this book from my bookshelf when I was preparing internal presentation about micro services for my Roche colleagues. I was mainly interested in Saga and Composite Front End patterns. But when I started, I decided to read rest of the book. Patterns Below you can find my...","categories": ["books"],
+        "tags": [],
+        "url": "/blog/2014/06/26/soa-patterns-book-review/",
+        "teaser": null
+      },{
+        "title": "Mastering Node.js - book review",
+        "excerpt":"Overview I’m really impressed by Node.js (and JavaScript) ecosystem. I took Mastering Node.js book to understand Node.js philosophy and compare to JVM world. V8 JavaScript virtual machine, conceptually very similar to JVM. The most important element of JavaScript ecosystem if you want to do something more than client side web...","categories": ["books"],
+        "tags": [],
+        "url": "/blog/2014/08/20/mastering-nodejs-book-review/",
+        "teaser": null
+      },{
+        "title": "Programming language does not matter",
+        "excerpt":"A few days ago I participated in quick presentation of significant e-commerce platform. The custom platform implemented mostly in PHP and designed as scalable and distributed system. And I was really impressed! Below you can find a short summary of chosen libraries, frameworks and tools. Symfony - The leading PHP...","categories": ["architecture","PHP"],
+        "tags": [],
+        "url": "/blog/2014/09/15/programming_language_does_not_matter/",
+        "teaser": null
+      },{
+        "title": "How to learn DDD",
+        "excerpt":"Books Domain Driven Design by Eric Evans. You have to read this book, period. From the very beginning to very end. Do not stop reading after first part of the book, the part about strategic design is much more important. Study this book again and again. I did not read...","categories": ["DDD","presentations"],
+        "tags": [],
+        "url": "/blog/2014/09/22/ddd-how-to-learn/",
+        "teaser": null
+      },{
+        "title": "Spark and Spark Streaming unit testing",
+        "excerpt":"When you develop distributed system, it is crucial to make it easy to test. Execute tests in controlled environment, ideally from your IDE. Long develop-test-develop cycle for complex systems could kill your productivity. Below you find my testing strategy for Spark and Spark Streaming applications. Unit or integration tests, that...","categories": ["spark","TDD","scala"],
+        "tags": [],
+        "url": "/blog/2015/03/01/spark-unit-testing/",
+        "teaser": null
+      },{
+        "title": "Spark and Kafka integration patterns, part 1",
+        "excerpt":"I published post on the allegro.tech blog, how to integrate Spark Streaming and Kafka. In the blog post you will find how to avoid java.io.NotSerializableException exception when Kafka producer is used for publishing results of the Spark Streaming processing. http://allegro.tech/spark-kafka-integration.html You could be also interested in the following part of...","categories": ["spark","kafka","scala"],
+        "tags": [],
+        "url": "/blog/2015/08/06/spark-kafka-integration1/",
+        "teaser": null
+      },{
+        "title": "Spark and Kafka integration patterns, part 2",
+        "excerpt":"In the world beyond batch, streaming data processing is a future of dig data. Despite of the streaming framework using for data processing, tight integration with replayable data source like Apache Kafka is often required. The streaming applications often use Apache Kafka as a data source, or as a destination...","categories": ["spark","kafka","scala"],
+        "tags": [],
+        "url": "/blog/2016/01/29/spark-kafka-integration2/",
+        "teaser": null
+      },{
+        "title": "Spark application assembly for cluster deployments",
+        "excerpt":"When I tried to deploy my first Spark application on a YARN cluster, I realized that there was no clear and concise instruction how to prepare the application for deployment. This blog post could be treated as missing manual on how to build Spark application written in Scala to get...","categories": ["scala","sbt","spark","kafka"],
+        "tags": [],
+        "url": "/blog/2016/03/11/spark-application-assembly/",
+        "teaser": null
+      },{
+        "title": "Long-running Spark Streaming jobs on YARN cluster",
+        "excerpt":"A long-running Spark Streaming job, once submitted to the YARN cluster should run forever until it is intentionally stopped. Any interruption introduces substantial processing delays and could lead to data loss or duplicates. Neither YARN nor Apache Spark have been designed for executing long-running services. But they have been successfully...","categories": ["spark","yarn","hdfs"],
+        "tags": [],
+        "url": "/blog/2016/09/30/spark-streaming-on-yarn/",
+        "teaser": null
+      },{
+        "title": "Apache BigData Europe Conference Summary",
+        "excerpt":"Last week I attended Apache Big Data Europe held in Sevilla, Spain. The event concentrates around big data projects under Apache Foundation umbrella. Below you can find my overall impression on the conference and notes from several interesting sessions. The notes are presented as a short checklists, if some aspect...","categories": ["conferences"],
+        "tags": [],
+        "url": "/blog/2016/11/18/apache-bigdata-europe/",
+        "teaser": null
+      },{
+        "title": "Kafka Streams DSL vs Processor API",
+        "excerpt":"Kafka Streams is a Java library for building real-time, highly scalable, fault tolerant, distributed applications. The library is fully integrated with Kafka and leverages Kafka producer and consumer semantics (e.g: partitioning, rebalancing, data retention and compaction). What is really unique, the only dependency to run Kafka Streams application is a...","categories": ["kafka","kafka streams","scala"],
+        "tags": [],
+        "url": "/blog/2017/11/02/kafka-streams-dsl-vs-processor-api/",
+        "teaser": null
+      }]
